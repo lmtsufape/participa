b37d0261ad97be45a26e7dfd6c9c1601a8d47eef
Ihago <ihagog12@gmail.com>
2020-02-20 15:39:08 -0300

    Tela Áreas

--- resources/views/coordenador/detalhesEvento.blade.php
+++ resources/views/coordenador/detalhesEvento.blade.php
@@ -67,9 +67,8 @@
         <div class="row">
             <div class="col-sm-12">
                 <h1 class="titulo-detalhes">Comissão</h1>
-            </div>            
+            </div>
         </div>
-
         <div class="row justify-content-center">
             <div class="col-sm-6">
                 <div class="card">
@@ -496,7 +495,7 @@
     </div>
 
     <div class="row justify-content-center">
-        <div class="col-sm-6">
+        <div class="col-sm-8">
             <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Nova Área</h5>
@@ -529,34 +528,38 @@
                   
                 </div>
               </div>{{-- End card--}}
-            
+        </div>        
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Áreas</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Áreas cadastradas no seu evento</h6>
+                  <p class="card-text">
+                    <table class="table table-hover table-responsive-lg table-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Remover</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($areas as $area)
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>{{$area->nome}}</td>
+                              <td>remover</td>
+                            </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                  </p>
+                </div>
+              </div>
         </div>
-        
-    </div>
-    <div class="row">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Remover</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($areas as $area)
-            <tr>
-              <th scope="row">1</th>
-              <td>{{$area->nome}}</td>
-              <td>remover</td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
-    <div class="row titulo">
-        <h1>Cadastrar Nova</h1>
-    </div>
-    
+    </div>    
 </div>
 
 <!-- Revisores -->
