0c03b6a24a155a7818ce81546cb2be6dcf07bc61
Ihago <ihagog12@gmail.com>
2020-02-20 15:34:12 -0300

    Tela Revisores

--- resources/views/coordenador/detalhesEvento.blade.php
+++ resources/views/coordenador/detalhesEvento.blade.php
@@ -565,75 +565,99 @@
     <div class="row">
         <div class="col-sm-12">
             <h1 class="titulo-detalhes">Revisores</h1>
+        </div>            
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Revisores</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Cadastre um novo revisor no seu evento</h6>
+                    <form method="POST" action="{{route('revisor.store')}}">
+                        @csrf
+                        <p class="card-text">
+                            <input type="hidden" name="eventoId" value="{{$evento->id}}">
+                            <div class="row justify-content-center">
+                                <div class="col-sm-6">
+                                    <label for="emailRevisor" class="col-form-label">{{ __('Email do Revisor') }}</label>
+                                    <input id="emailRevisor" type="text" class="form-control @error('emailRevisor') is-invalid @enderror" name="emailRevisor" value="{{ old('emailRevisor') }}" required autocomplete="emailRevisor" autofocus>
+                    
+                                    @error('emailRevisor')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="col-sm-6">
+                                    <label for="areaRevisor" class="col-form-label">{{ __('Área') }}</label>
+                                    <select class="form-control @error('areaRevisor') is-invalid @enderror" id="areaRevisor" name="areaRevisor">
+                                        <option value="" disabled selected hidden>-- Área --</option>
+                                        @foreach($areas as $area)
+                                        <option value="{{$area->id}}">{{$area->nome}}</option>
+                                        @endforeach
+                                    </select>
+                    
+                                    @error('areaRevisor')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            
+                        </p>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary" style="width:100%">
+                                    {{ __('Finalizar') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Revisores</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Revisores cadastrados no seu evento</h6>
+                  <p class="card-text">
+                    <table class="table table-hover table-responsive-lg table-sm">
+                        <thead>
+                          <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Área</th>
+                            <th scope="col">Em Andamento</th>
+                            <th scope="col">Finalizados</th>
+                            <th scope="col">Ultimo Prazo</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($revisores as $revisor)
+                            <tr>
+                              <td>{{$revisor->user->nome}}</td>
+                              <td>{{$revisor->area->nome}}</td>
+                              <td>{{$revisor->trabalhosCorrigidos}}</td>
+                              <td>{{$revisor->correcoesEmAndamento}}</td>
+                              <td>{{$revisor->prazo}}</td>
+                            </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                  </p>
+                  
+                </div>
+              </div>
         </div>
     </div>
     
-    <div class="row">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Nome</th>
-            <th scope="col">Área</th>
-            <th scope="col">Em Andamento</th>
-            <th scope="col">Finalizados</th>
-            <th scope="col">Ultimo Prazo</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($revisores as $revisor)
-            <tr>
-              <td>{{$revisor->user->nome}}</td>
-              <td>{{$revisor->area->nome}}</td>
-              <td>{{$revisor->trabalhosCorrigidos}}</td>
-              <td>{{$revisor->correcoesEmAndamento}}</td>
-              <td>{{$revisor->prazo}}</td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
-    <div class="row titulo">
-        <h1>Cadastrar Novo</h1>
-    </div>
-    <form method="POST" action="{{route('revisor.store')}}">
-      @csrf
-      <input type="hidden" name="eventoId" value="{{$evento->id}}">
-      <div class="row justify-content-center">
-          <div class="col-sm-6">
-              <label for="emailRevisor" class="col-form-label">{{ __('Email do Revisor') }}</label>
-              <input id="emailRevisor" type="text" class="form-control @error('emailRevisor') is-invalid @enderror" name="emailRevisor" value="{{ old('emailRevisor') }}" required autocomplete="emailRevisor" autofocus>
-
-              @error('emailRevisor')
-              <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-              </span>
-              @enderror
-          </div>
-          <div class="col-sm-6">
-              <label for="areaRevisor" class="col-form-label">{{ __('Área') }}</label>
-              <select class="form-control @error('areaRevisor') is-invalid @enderror" id="areaRevisor" name="areaRevisor">
-                  <option value="" disabled selected hidden>-- Área --</option>
-                  @foreach($areas as $area)
-                    <option value="{{$area->id}}">{{$area->nome}}</option>
-                  @endforeach
-              </select>
-
-              @error('areaRevisor')
-              <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-              </span>
-              @enderror
-          </div>
-      </div>
-      <div class="row justify-content-center">
-
-          <div class="col-md-6">
-              <button type="submit" class="btn btn-primary" style="width:100%">
-                  {{ __('Finalizar') }}
-              </button>
-          </div>
-      </div>
-    </form>
+    
 
 
 </div>
