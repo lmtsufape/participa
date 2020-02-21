91d07d01aafeb18ee525e10ffa6f6b82202676fb
Ihago <ihagog12@gmail.com>
2020-02-21 13:20:37 -0300

    card evento

--- resources/views/coordenador/home.blade.php
+++ resources/views/coordenador/home.blade.php
@@ -26,38 +26,70 @@
                 <img src="{{asset('img/colorscheme.png')}}" class="card-img-top" alt="...">
                 
                 <div class="card-body">
-                    <h4 class="card-title">{{$evento->nome}}</h4>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4 class="card-title">
+                                <div class="row justify-content-center">
+                                    <div class="col-sm-12">
+                                        {{$evento->nome}}
+                                        @can('isCoordenador', $evento)
+                                            <div class="btn-group dropright dropdown-options">
+                                                <a id="options" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    {{-- <img src="{{asset('img/icons/ellipsis-v-solid.svg')}}" style="width:8px"> --}}
+                                                </a>
+                                                <div class="dropdown-menu">
+                                                    <a href="{{ route('coord.detalhesEvento', ['eventoId' => $evento->id]) }}" class="dropdown-item">
+                                                        <img src="{{asset('img/icons/eye-regular.svg')}}" class="icon-card" alt="">
+                                                        Detalhes
+                                                    </a>
+                                                    <a href="{{route('evento.editar',$evento->id)}}" class="dropdown-item">
+                                                        <img src="{{asset('img/icons/edit-regular.svg')}}" class="icon-card" alt="">
+                                                        Editar
+                                                    </a>
+                                                    <form method="POST" action="{{route('evento.deletar',$evento->id)}}">
+                                                        {{ csrf_field() }}
+                                                        {{ method_field('DELETE') }}
+                                                        <button type="submit" class="dropdown-item">
+                                                            <img src="{{asset('img/icons/trash-alt-regular.svg')}}" class="icon-card" alt="">
+                                                            Deletar
+                                                        </button>
+                                                        
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        @endcan
+                                    </div>    
+                                    
+                                </div>
+                            
+                            </h4>
+                            
+                        </div>
+                    </div>
                     <p class="card-text">
                         <strong>Início:</strong> {{$evento->dataInicio}}<br>
                         <strong>Fim:</strong> {{$evento->dataFim}}<br>
                         <strong>Número de Vagas:</strong> {{$evento->numeroParticipantes}}
                     </p>
 
-                    @can('isCoordenador', $evento)
+                    
                       <div class="row justify-content-center">
                           <div class="col-sm-4">
                               <div class="row justify-content-center">
-                                  <a href="{{ route('coord.detalhesEvento', ['eventoId' => $evento->id]) }}" class="btn btn-primary">Detalhes</a>
+                                  
                               </div>
                           </div>
                           <div class="col-sm-4">
                               <div class="row justify-content-center">
-                                  <a href="{{route('evento.editar',$evento->id)}}" class="btn btn-secondary">Editar</a>
+                                  
 
                               </div>
                           </div>
                           <div class="col-sm-4" style="padding:0">
-                              <form method="POST" action="{{route('evento.deletar',$evento->id)}}">
-                                  {{ csrf_field() }}
-                                  {{ method_field('DELETE') }}
-                                  <div class="row justify-content-center">
-                                      <button type="submit" class="btn btn-danger">Deletar</button>
-
-                                  </div>
-                              </form>
+                              
                           </div>
                       </div>
-                    @endcan
+                    
                     
                 </div>
                 
