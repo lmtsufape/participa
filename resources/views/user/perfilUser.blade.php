5296e68d64f75205c38a03a0c26f8c0e2f51aa4f
Ihago <ihagog12@gmail.com>
2020-02-28 14:39:30 -0300

    perfil user

--- resources/views/user/perfilUser.blade.php
+++ resources/views/user/perfilUser.blade.php
@@ -17,7 +17,7 @@
         @csrf
         <div class="row justify-content-center">
             <input hidden name="id" value="{{$user->id}}">
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <label for="name" class="col-form-label">{{ __('Name') }}</label>
                 <input value="{{$user->name}}" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
@@ -28,7 +28,7 @@
                 @enderror
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <label for="cpf" class="col-form-label">{{ __('CPF') }}</label>
                 <input value="{{$user->cpf}}" id="cpf" type="text" class="form-control @error('cpf') is-invalid @enderror" name="cpf" value="{{ old('cpf') }}" required autocomplete="cpf" autofocus>
 
@@ -39,20 +39,10 @@
                 @enderror
             </div>
 
-            <div class="col-md-3">
-                <label for="celular" class="col-form-label">{{ __('Celular') }}</label>
-                <input value="{{$user->celular}}" id="celular" type="number" class="form-control @error('celular') is-invalid @enderror" name="celular" value="{{ old('celular') }}" required autocomplete="celular" autofocus>
-
-                @error('celular')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
         </div>
         <div class="row justify-content-center">
             
-            <div class="col-md-6">
+            <div class="col-md-8">
               <label for="instituicao" class="col-form-label">{{ __('Instituição de Ensino') }}</label>
               <input value="{{$user->instituicao}}" id="instituicao" type="text" class="form-control @error('instituicao') is-invalid @enderror" name="instituicao" value="{{ old('instituicao') }}" required autocomplete="instituicao" autofocus>
 
@@ -63,16 +53,17 @@
               @enderror
             </div>
 
-            <div class="col-md-6">
-                <label for="especProfissional" class="col-form-label">{{ __('Esperiência Profissional') }}</label>
-                <input value="{{$user->especProfissional}}" id="especProfissional" type="text" class="form-control @error('especProfissional') is-invalid @enderror" name="especProfissional" value="{{ old('especProfissional') }}" required autocomplete="especProfissional" autofocus>
+            <div class="col-md-4">
+                <label for="celular" class="col-form-label">{{ __('Celular') }}</label>
+                <input value="{{$user->celular}}" id="celular" type="number" class="form-control @error('celular') is-invalid @enderror" name="celular" value="{{ old('celular') }}" required autocomplete="celular" autofocus>
 
-                @error('especProfissional')
+                @error('celular')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
+
         </div>
 
         <div class="row subtitulo" style="margin-top:20px">   
@@ -139,16 +130,47 @@
                       </span>
                   @enderror
             </div>
-            <div class="col-md-4">
+            <div class="col-sm-4">
                 <label for="uf" class="col-form-label">{{ __('UF') }}</label>
-                <input value="{{$end->uf}}" id="uf" type="text" class="form-control @error('uf') is-invalid @enderror" name="uf" required autocomplete="uf">
+                {{-- <input id="uf" type="text" class="form-control @error('uf') is-invalid @enderror" name="uf" value="{{ old('uf') }}" required autocomplete="uf" autofocus> --}}
+                <select class="form-control @error('uf') is-invalid @enderror" id="uf" name="uf">
+                    <option value="" disabled selected hidden>-- UF --</option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                </select>
 
                 @error('uf')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
                 @enderror
             </div>
+            
           </div>
           <div class="row justify-content-center" style="margin: 20px 0 20px 0">
 
