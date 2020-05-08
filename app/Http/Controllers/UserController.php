f7e853b8df26bfdd31022f06d5acff4d956b27c4
paulo <paulosousa1011@gmail.com>
2020-05-08 17:02:53 -0300

    Login de usuario temporario funcional e Edicao de dados de usuario temporario tambem funcional

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
         return view('user.perfilUser',['user'=>$user,'end'=>$end]);
     }
     function editarPerfil(Request $request){
-        // dd($request->name);
+        // dd($request->id);
 
         $validator = $request->validate([
             'name' => 'required|string|max:255',
@@ -36,14 +36,31 @@ class UserController extends Controller
         ]);
 
         if(Auth()->user()->usuarioTemp == true){
-          // endereço
-          $end = new Endereco();
-          $end->rua = $data['rua'];
-          $end->numero = $data['numero'];
-          $end->bairro = $data['bairro'];
-          $end->cidade = $data['cidade'];
-          $end->uf = $data['uf'];
-          $end->cep = $data['cep'];
+
+            // criar endereço
+            $end = new Endereco();
+            $end->rua = $request->input('rua');
+            $end->numero = $request->input('numero');
+            $end->bairro = $request->input('bairro');
+            $end->cidade = $request->input('cidade');
+            $end->uf = $request->input('uf');
+            $end->cep = $request->input('cep');
+
+            $end->save();
+
+            // Atualizar dados não preenchidos de User
+            $user = User::find($request->id);
+            $user->name = $request->input('name');
+            $user->cpf = $request->input('cpf');
+            $user->celular = $request->input('celular');
+            $user->instituicao = $request->input('instituicao');
+            // $user->especProfissional = $request->input('especProfissional');
+            $user->usuarioTemp = null;
+            $user->enderecoId = $end->id;
+            $user->save();
+
+            return view('home');
+            
         }
         // User
         $user = User::find($request->id);
@@ -69,6 +86,50 @@ class UserController extends Controller
         return view('home');
 
 
+
+        // return view('index');
+    }
+
+    function editarPerfilTemp(Request $request){
+        // dd($request->id);
+
+        $validator = $request->validate([
+            'name' => 'required|string|max:255',
+            'cpf' => 'required',
+            'celular' => 'required|string',
+            'instituicao' => 'required|string| max:255',
+            // 'especProfissional' => 'nullable|string',
+            'rua' => 'required|string|max:255',
+            'numero' => 'required|string',
+            'bairro' => 'required|string|max:255',
+            'cidade' => 'required|string|max:255',
+            'uf' => 'required|string',
+            'cep' => 'required|integer',
+        ]);
+
+        
+        // User
+        $user = User::find($request->id);
+        $user->name = $request->input('name');
+        $user->cpf = $request->input('cpf');
+        $user->celular = $request->input('celular');
+        $user->instituicao = $request->input('instituicao');
+        // $user->especProfissional = $request->input('especProfissional');
+        $user->usuarioTemp = null;
+        $user->save();
+
+        // endereço
+        $end = Endereco::find($user->enderecoId);
+        $end->rua = $request->input('rua');
+        $end->numero = $request->input('numero');
+        $end->bairro = $request->input('bairro');
+        $end->cidade = $request->input('cidade');
+        $end->uf = $request->input('uf');
+        $end->cep = $request->input('cep');
+
+        $end->save();
+        // dd([$user,$end]);
+        return redirect(route("coord.home"));
 
         // return view('index');
     }
--- resources/views/user/perfilUser.blade.php
+++ resources/views/user/perfilUser.blade.php
@@ -201,7 +201,7 @@
         </div>
     </div>
 
-    <form method="POST" action="{{ route('perfil') }}">
+    <form method="POST" action="{{ route('perfil.userTemp') }}">
         @csrf
         <div class="row justify-content-center">
             <input hidden name="id" value="{{$user->id}}">
