446fa2a66ddf8d2ac85e33618a470823ee3802ce
carlos <carlos@debian.carlos>
2020-11-10 15:23:29 -0300

    visualizar programacao

--- resources/views/coordenador/programacao/inscricoes.blade.php
+++ resources/views/coordenador/programacao/inscricoes.blade.php
@@ -39,23 +39,24 @@
                                                 <th>Identificador</th>
                                                 <th>Quantidade total/aplicada</th>
                                                 <th>Valor pago/recebido</th>
-                                                <th>Visualizar</th>
-                                                <th>Excluir</th>
+                                                <th style="text-align:center">Visualizar</th>
+                                                <th style="text-align:center">Excluir</th>
                                             </th>
                                         </thead>
                                         @foreach ($promocoes as $promocao)
-                                            {{-- <tbody>
+                                            <tbody>
                                                 <th>
-                                                    <td data-toggle="modal" data-target="#modalAtividadeEdit{{$atv->id}}">{{$atv->titulo}}</td>
-                                                    <td data-toggle="modal" data-target="#modalAtividadeEdit{{$atv->id}}">{{$atv->tipoAtividade->descricao}}</td>
-                                                    <td data-toggle="modal" data-target="#modalAtividadeEdit{{$atv->id}}">@if(empty($atv->vagas)) Ilimitado @else {{$atv->vagas}} @endif</td>
-                                                    <td data-toggle="modal" data-target="#modalAtividadeEdit{{$atv->id}}">@if(empty($atv->valor)) Grátis @else R$ {{$atv->valor}},00 @endif</td>
-                                                    <td data-toggle="modal" data-target="#modalAtividadeEdit{{$atv->id}}">{{$atv->local}}</td>
-                                                    <td data-toggle="modal" data-target="#modalAtividadeEdit{{$atv->id}}">@if(empty($atv->carga_horaria)) Nenhuma @else {{$atv->carga_horaria}} @endif</td>
-                                                    <td><input id="checkbox_{{$atv->id}}" type="checkbox" @if($atv->visibilidade_participante) checked @endif onclick="setVisibilidadeAtv({{$atv->id}})"></td>
-                                                    <td data-toggle="modal" data-target="#modalExcluirAtividade{{$atv->id}}"><button style="border: none; background-color: rgba(255, 255, 255, 0);"><img src="{{asset('img/icons/trash-alt-regular.svg')}}" class="icon-card" alt=""></button></td>
+                                                    <td>{{$promocao->identificador}}</td>
+                                                    <td>Pendencia para programar</td>
+                                                    @if ($promocao->valor == null || $promocao->valor <= 0)
+                                                        <td>Grátis</td>
+                                                    @else 
+                                                        <td>R$ {{number_format($promocao->valor, 2,',','.')}} / R$ {{number_format($promocao->valor - $promocao->valor * 0.10, 2,',','.')}}</td>
+                                                    @endif
+                                                    <td style="text-align:center"><a href="#" data-toggle="modal" data-target="#modalPromocaoEdit{{$promocao->id}}" ><img src="{{asset('img/icons/eye-regular.svg')}}" style="width:20px"></a></td>
+                                                    <td style="text-align:center"><a href="#" data-toggle="modal" data-target="#modalPromocaoDelete{{$promocao->id}}"><img src="{{asset('img/icons/trash-alt-regular.svg')}}" class="icon-card" alt=""></a></td>
                                                 </th>
-                                            </tbody> --}}
+                                            </tbody>
                                         @endforeach
                                     </table>
                                 </p>
@@ -118,7 +119,7 @@
     </div>
 </div>
 
-<!-- Modal -->
+<!-- Modal criar promocao -->
 <div class="modal fade modal-example-lg" id="modalCriarPromocao" tabindex="-1" role="dialog" aria-labelledby="modalCriarPromocaoLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
@@ -319,4 +320,107 @@
       </div>
     </div>
 </div>
+{{-- Fim do modal criar promoção --}}
+{{-- Modal visualizar promocao --}}
+@foreach ($promocoes as $promocao)
+<div class="modal fade modal-example-lg" id="modalPromocaoEdit{{$promocao->id}}" tabindex="-1" role="dialog" aria-labelledby="modalPromocaoEdit{{$promocao->id}}Label" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header" style="background-color: #114048ff; color: white;">
+          <h5 class="modal-title" id="modalPromocaoEdit{{$promocao->id}}Label">{{$promocao->identificador}}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="color: white;">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h5>Valor que o inscrito irá pagar</h5>
+                        @if ($promocao->valor == null || $promocao->valor <= 0)
+                            <p>A promoção é gratuita</p>
+                        @else
+                            <p>R$ {{number_format($promocao->valor, 2,',','.')}}</p>
+                        @endif
+                    </div>
+                    <div class="col-sm-6">
+                        <h5>Valor recebido</h5>
+                        @if ($promocao->valor == null || $promocao->valor <= 0)
+                            <p>A promoção é gratuita</p>
+                        @else
+                            <p>R$ {{number_format($promocao->valor - $promocao->valor * 0.10, 2,',','.')}}*<br><span style="font-size: 10px;">*Taxa de 10%</span></p>
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h5>Descrição</h5>
+                        <textarea class="form-control" id="" disabled>{{$promocao->descricao}}</textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <hr>
+                        <h4>Lotes</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    @foreach ($promocao->lotes as $lote)
+                        <div class="col-sm-3">
+                            <h5>Data início</h5>
+                            <p>
+                                {{date('d/m/Y',strtotime($lote->inicio_validade))}}
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <h5>Data final</h5>
+                            <p>
+                                {{date('d/m/Y',strtotime($lote->fim_validade))}}
+                            </p>
+                        </div>
+                        <div class="col-sm-5">
+                            <h5>Quantidade disponivel/aplicada</h5>
+                            <p>
+                                {{$lote->quantidade_de_aplicacoes}}/Pendencia para programar
+                            </p>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <hr>
+                        <h4>Atividades inclusas na promoção</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <table class="table table-hover table-responsive-lg table-sm" style="position: relative;">
+                        <thead>
+                            <th>
+                                <th>Título</th>
+                                <th>Local</th>
+                                <th>Valor original</th>
+                            </th>
+                        </thead>
+                        @foreach ($promocao->atividades()->orderBy('titulo')->get() as $atv)
+                            <tbody>
+                                <th>
+                                    <td>{{$atv->titulo}}</td>
+                                    <td>{{$atv->local}}</td>
+                                    @if ($atv->valor == null || $atv->valor <= 0)
+                                        <th>Grátis</th>
+                                    @else 
+                                        <th>R$ {{number_format($atv->valor, 2,',','.')}}</th>
+                                    @endif
+                                </th>
+                            </tbody>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+</div>    
+@endforeach
+{{-- Fim modal visualizar promocao --}}
 @endsection
