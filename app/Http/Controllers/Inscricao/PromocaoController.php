1aadcacb129e5d92da040ca0406dd97fb4fc9fc7
carlos <carlos@debian.carlos>
2020-11-11 15:31:36 -0300

    adicionando model ao promocao controller

--- app/Http/Controllers/Inscricao/PromocaoController.php
+++ app/Http/Controllers/Inscricao/PromocaoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Inscricao\Promocao;
 use App\Models\Inscricao\Lote;
+use App\Evento;
 
 class PromocaoController extends Controller
 {
