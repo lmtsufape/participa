4fcc6d1cb00eda3e51da35c147a2e3e91159921e
Matheus Machado <matheusmachado708@gmail.com>
2021-03-04 19:13:08 -0300

    Botão de marcar ou desmarcar em listar revisores
    - Botão de marcar ou desmarcar todas as áreas em novo revisor
    - Botão de marcar ou desmarcar todas as modalidades em novo revisor
    
    - Botão de marcar ou desmarcar todas as áreas em editar revisor
    - Botão de marcar ou desmarcar todas as modalidades em editar revisor

---
+++ public/js/js_listar_revisores.js
@@ -0,0 +1,11 @@
+function marcar_desmarcar_todos_checkbox_por_classe(checkbox_marcar_desmarcar, nome_classe)  {
+    // se o checkbox foi marcado itera por todos os elementos com a classe nome_classe
+    // e marca todos eles como sim, se foi desmarcado, desmarca todo mundo
+    let valor_da_marcacao = checkbox_marcar_desmarcar.checked;
+    let checkboxes_areas = document.getElementsByClassName(nome_classe);
+
+    for(let checkbox of checkboxes_areas) {
+        checkbox.checked = valor_da_marcacao;
+    }
+       
+}
