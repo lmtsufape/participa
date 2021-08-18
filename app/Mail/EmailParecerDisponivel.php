3fa5cb669cd5d0713881895c3d866163ea64359e
KelwinJonas <kelwinjonas@gmail.com>
2021-08-18 19:08:57 -0300

    Ajustes e email de parecer disponivel

---
+++ app/Mail/EmailParecerDisponivel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EmailParecerDisponivel extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $evento;
+    public $trabalho;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($evento, $trabalho)
+    {
+      $this->evento      = $evento;
+      $this->trabalho    = $trabalho;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+      return  $this->from('lmtsteste@gmail.com', 'Easy ')
+                    ->subject("Sistema Easy - Parecer disponível")
+                    ->markdown('emails.emailParecerDisponivel')->with([
+                      'evento' => $this->evento,
+                      'senha'  => $this->trabalho,
+                  ]);
+    }
+}
--- resources/views/emails/emailParecerDisponivel.blade.php
+++ resources/views/emails/emailParecerDisponivel.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
-# Olá {{ $user->name }}!
+# Olá {{ $trabalho->autor->name }}!
 
 
 
-O trabalho ou atividade {{ $trabalho->titulo }}, submetido por {{ $autor->name }}, no evento {{ $nomeEvento }}, acaba de ser avaliado pelo revisor {{ $revisor->user->name }}.
+O parecer do seu trabalho ou atividade "{{ $trabalho->titulo }}", submetido no evento "{{ $evento->nome }}", está disponível.
 
-Acesse o sistema para verificar a avaliação.
+Acesse o sistema para verificar o parecer.
 
 @component('mail::button', ['url' => 'http://sistemas.ufape.edu.br/easy'])
 Acessar o sistema
