8622dca1496493c883fe8515367e6268b13c0e8e
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2021-10-11 13:19:46 -0300

    diminui o padding bottom do footer

--- resources/views/componentes/footer.blade.php
+++ resources/views/componentes/footer.blade.php
@@ -1,8 +1,8 @@
-<div class="footer">
+<div class="footer" style="padding-bottom: 0px;">
   <div class="row justify-content-center links-rodape">
-      <a href="{{ route('index') }}">Inicio</a>  |  
-      <a href="#">Eventos Acadêmicos</a>  |  
-      <a hfer="#" data-toggle="modal" data-target="#modalInfo" >Sobre</a> 
+      <a href="{{ route('index') }}">Inicio</a>  |
+      <a href="#">Eventos Acadêmicos</a>  |
+      <a hfer="#" data-toggle="modal" data-target="#modalInfo" >Sobre</a>
   </div>
   <div class="row" style="margin-bottom: -3.5rem;">
     <div class="col-sm-12 separador">
