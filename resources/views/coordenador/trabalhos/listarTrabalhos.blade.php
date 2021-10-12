49f05f8263072735ee83ff363c1f1d88700dfb4d
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2021-10-12 08:10:56 -0300

    corrige palavra distribuir

--- resources/views/coordenador/trabalhos/listarTrabalhos.blade.php
+++ resources/views/coordenador/trabalhos/listarTrabalhos.blade.php
@@ -225,7 +225,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header" style="background-color: #114048ff; color: white;">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Distrbuir trabalhos automaticamente</h5>
+        <h5 class="modal-title" id="exampleModalCenterTitle">Distribuir trabalhos automaticamente</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="color: white;">
           <span aria-hidden="true">&times;</span>
         </button>
