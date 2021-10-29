cf393a39cc1c2816b46ed6eff1ce11a8a4f9854f
carlos1270 <carlos.andre1270@gmail.com>
2021-10-29 11:29:50 -0300

    ajuste

--- resources/views/coordenador/revisores/listarRevisores.blade.php
+++ resources/views/coordenador/revisores/listarRevisores.blade.php
@@ -68,7 +68,7 @@
                                               Enviar e-mail
                                           </button>
                                       </form> --}}
-                                      @component('componentes.modal', ['email' => $revisor->email, 'evento' => $evento])
+                                      @component('componentes.modal', ['revisor' => $revisor, 'evento' => $evento])
 
                                       @endcomponent
                                   </td>
