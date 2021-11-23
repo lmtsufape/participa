c517af3c83687635e5b453bf2865ee7edce7c757
KelwinJonas <kelwinjonas@gmail.com>
2021-11-23 11:49:20 -0300

    Retirar acesso ao site em email de certificado

--- resources/views/emails/emailEnviarCertificado.blade.php
+++ resources/views/emails/emailEnviarCertificado.blade.php
@@ -7,9 +7,6 @@ Agradecemos a sua contribuição como {{ $cargo }} no evento {{ $nomeEvento }}.
 
 Segue anexo da sua certificação.
 
-@component('mail::button', ['url' => 'http://sistemas.ufape.edu.br/easy'])
-Acessar o sistema
-@endcomponent
 
 @include('emails.footer')
 @endcomponent
