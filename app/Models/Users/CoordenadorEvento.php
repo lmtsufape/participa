de4df38bdd91a91857ae3d976e04aa3af6bfadba
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-09-30 10:05:28 -0300

    usa a classe como tabela intermediaria na relação usuário coordena evento

--- app/Models/Users/CoordenadorEvento.php
+++ app/Models/Users/CoordenadorEvento.php
@@ -2,17 +2,26 @@
 
 namespace App\Models\Users;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class CoordenadorEvento extends Model
+class CoordenadorEvento extends Pivot
 {
-    public function evento()
-    {
-        return $this->hasMany('App\Models\Submissao\Evento', 'coordenadorId');
-    }
+    protected $table = 'coordenador_eventos';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'user_id',
+        'eventos_id',
+    ];
 
     public function user()
     {
         return $this->belongsTo('App\Models\Users\User');
+    }
+
+    public function evento()
+    {
+        return $this->belongsTo('App\Models\Submissao\Evento', 'eventos_id');
     }
 }
