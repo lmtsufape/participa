eb1b1c1512f72577a965ff0eeed083458f1cd04b
KelwinJonas <kelwinjonas@gmail.com>
2022-10-13 13:38:09 -0300

    nome da rota de visualizar evento

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/', function () {
 })->name('cancelarCadastro');
 
 Route::namespace('Submissao')->group(function () {
-  Route::get('/evento/visualizar/{id}','EventoController@show')->name('evento.visualizar');
+  Route::get('/evento/{id}','EventoController@show')->name('evento.visualizar');
   Route::view('validarCertificado', 'validar')->name('validarCertificado');
   Route::post('validarCertificado', 'CertificadoController@validar')->name('validarCertificadoPost');
   Route::get('/home', 'Submissao\CertificadoController@validar')->name('home')->middleware('verified', 'isTemp');
