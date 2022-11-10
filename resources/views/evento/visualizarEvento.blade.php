e926e84037ef8c3964f7af75b54b20b06440b35d
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-11-10 14:04:50 -0300

    desabilita o botão ao submeter

--- resources/views/evento/visualizarEvento.blade.php
+++ resources/views/evento/visualizarEvento.blade.php
@@ -372,7 +372,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                        <button type="submit" class="btn btn-primary">Confirmar</button>
+                        <button type="submit" class="btn btn-primary button-prevent-multiple-submits">Confirmar</button>
                     </div>
                 </form>
             </div>
