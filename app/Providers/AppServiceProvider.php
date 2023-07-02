c73f690b7e781692bce8a3f4a81ff4fb52dbc98a
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-07-02 09:25:21 -0300

    custom validations que tinham faltado do upgrade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Validator::extend('telefone', '\App\Utils\TelefoneValidation@validate', 'Celular inválido');
+        Validator::extend('time', '\App\Utils\TimeValidation@validate', 'Hora inválida');
     }
 }
