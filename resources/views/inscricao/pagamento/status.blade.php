8cb1e12b1868e88f1163164bc2da322fc9183f15
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-07-30 07:27:31 -0300

    telas de pagamento e status

---
+++ resources/views/inscricao/pagamento/status.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container w-50"><div id="statusScreenBrick_container"></div></div>
+@endsection
+
+@section('javascript')
+    @parent
+    <script src="https://sdk.mercadopago.com/js/v2"></script>
+    <script>
+        const mp = new MercadoPago('', {
+            locale: 'pt-BR'
+        });
+        const bricksBuilder = mp.bricks();
+        const pagamento = @json($pagamento);
+        const renderStatusScreenBrick = async (bricksBuilder) => {
+            const settings = {
+                initialization: {
+                    paymentId: pagamento.codigo, // Payment identifier, from which the status will be checked
+                },
+                customization: {
+                    visual: {
+                        style: {
+                            customVariables: {
+                                "baseColor": "#114048",
+                            },
+                            theme: "bootstrap",
+                        }
+                    },
+                    backUrls: {
+                    }
+                },
+                callbacks: {
+                    onReady: () => {
+                        // Callback called when Brick is ready
+                    },
+                    onError: (error) => {
+                        // Callback called for all Brick error cases
+                    },
+                },
+            };
+            window.statusScreenBrickController = await bricksBuilder.create('statusScreen',
+                'statusScreenBrick_container', settings);
+        };
+        renderStatusScreenBrick(bricksBuilder);
+    </script>
+@endsection
