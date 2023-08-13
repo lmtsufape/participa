c9afbdfebea9019ba51ecf33678ff2d63db37c2d
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-08-13 09:11:12 -0300

    categorias podem barrar submissao

---
+++ app/Http/Requests/StoreCategoriaParticipanteRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Submissao\Evento;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoriaParticipanteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $evento = Evento::find($this->evento_id);
+        return $evento && $this->user()->can('isCoordenadorOrCoordenadorDaComissaoOrganizadora', $evento);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required',
+            'valor_total' => 'required|numeric|min:0',
+            'tipo_valor.*' => 'nullable',
+            'valorDesconto.*' => 'required_with:tipo_valor.*|numeric|min:0',
+            'inícioDesconto.*' => 'required_with:tipo_valor.*|date',
+            'fimDesconto.*' => 'required_with:tipo_valor.*|date|after:inícioDesconto.*',
+            'permite_submissao' => 'boolean',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'valor_total.min' => 'Digite um valor positivo ou 0 para gratuito.',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'valorDesconto.*' => 'O valor',
+            'permite_submissao' => 'permite submissão'
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'permite_submissao' => $this->has('permite_submissao'),
+        ]);
+    }
+}
