eba5e729ae0aa529c971a2ebe0b9c163717840ce
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-03-14 12:39:41 -0300

    salva com a extensão original

--- app/Http/Controllers/Inscricao/InscricaoController.php
+++ app/Http/Controllers/Inscricao/InscricaoController.php
@@ -488,7 +488,8 @@ class InscricaoController extends Controller
                 } elseif ($campo->tipo == 'text' && $request->input('text-'.$campo->id) != null) {
                     $inscricao->camposPreenchidos()->attach($campo->id, ['valor' => $request->input('text-'.$campo->id)]);
                 } elseif ($campo->tipo == 'file' && $request->file('file-'.$campo->id) != null) {
-                    $path = Storage::putFileAs('eventos/'.$inscricao->evento->id.'/inscricoes/'.$inscricao->id.'/'.$campo->id, $request->file('file-'.$campo->id), $campo->titulo.'.pdf');
+                    $extensao = $request->file('file-'.$campo->id)->getClientOriginalExtension();
+                    $path = Storage::putFileAs('eventos/'.$inscricao->evento->id.'/inscricoes/'.$inscricao->id.'/'.$campo->id, $request->file('file-'.$campo->id), $campo->titulo.'.'.$extensao);
 
                     $inscricao->camposPreenchidos()->attach($campo->id, ['valor' => $path]);
                 } elseif ($campo->tipo == 'date' && $request->input('date-'.$campo->id) != null) {
