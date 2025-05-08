b7fd97077b47a17d13b2081f46119c3a14b46a08
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2025-05-07 23:40:39 -0300

    Aplicação do novo layout do login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--  <link href="{{ asset('css/style.css') }}" rel="stylesheet"> --}}
 
     <link href="{{ asset('css/styleIndex.css') }}" rel="stylesheet">
     <link href="{{ asset('css/dark-mode.css') }}" rel="stylesheet">
@@ -59,7 +59,7 @@
     @else
         @include('componentes.mensagens')
 
-        <main class="flex-grow-1 my-5">
+        <main class="flex-grow-1 py-5 bg-my-shadow">
             @yield('content')
         </main>
 
