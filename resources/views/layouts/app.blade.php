a85d2fa9f6b9615efd6d70935c109d55518b4be7
Douglas Filipe <batista.douglasfs@gmail.com>
2025-05-09 13:50:03 -0300

    fix: redesign da tela de cadastro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,10 @@
 
     <!--CSS DINAMICO-->
     @yield('css')
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 </head>
 
 <body class="d-flex flex-column min-vh-100" style="background-color: #f5f5f5;">
