ba6aba865aa2d26281ce48f77c9013debc7b951f
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2025-05-11 00:16:15 -0300

    Correção dos icones

--- resources/views/coordenador/index.blade.php
+++ resources/views/coordenador/index.blade.php
@@ -60,21 +60,28 @@
                                     </p>
                                     <div class="row">
                                         <div class="col-md-4">
-                                            <a class="d-flex flex-column align-items-center text-decoration-none" href="{{route('evento.visualizar',['id'=>$evento->id])}}">
-                                                <i class="far fa-eye" style="color: #008E3B"></i>
+                                            <a class="d-flex flex-column align-items-center text-success text-decoration-none" href="{{route('evento.visualizar',['id'=>$evento->id])}}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                                                </svg>
                                                 <span style="font-size: 10px"><strong>{{__('Visualizar evento')}}</strong></span>
                                             </a>
                                         </div>
                                         <div class="col-md-4">
-                                            <a class="d-flex flex-column align-items-center text-decoration-none" href="{{ route('coord.detalhesEvento', ['eventoId' => $evento->id]) }}">
-                                                <i class="fas fa-cog" style="color: #727272"></i>
+                                            <a class="d-flex flex-column align-items-center text-secondary text-decoration-none" href="{{ route('coord.detalhesEvento', ['eventoId' => $evento->id]) }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                                                    <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                                                </svg>
                                                 <span style="font-size: 10px"><strong>{{__('Configurar evento')}}</strong></span>
                                             </a>
                                         </div>
                                         @can('isCriador', $evento)
                                         <div class="col-md-4">
-                                            <a class="d-flex flex-column align-items-center text-decoration-none" href="#" data-toggle="modal" data-target="#modalExcluirEvento{{$evento->id}}">
-                                                <i class="far fa-trash-alt" style="color: #FF0000"></i>
+                                            <a class="d-flex flex-column align-items-center text-danger text-decoration-none" href="#" data-toggle="modal" data-target="#modalExcluirEvento{{$evento->id}}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                                                </svg>
                                                 <span style="font-size: 10px"><strong>{{__('Deletar evento')}}</strong></span>
                                             </a>
                                         </div>
