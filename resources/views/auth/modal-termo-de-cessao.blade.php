b6dd41cade9e9f924bc9da8de1b0e5da904aaea8
airton23 <airtondasf2305@gmail.com>
2025-06-08 23:34:47 -0300

    Ajustes e justificando o texto dos termos

--- resources/views/auth/modal-termo-de-cessao.blade.php
+++ resources/views/auth/modal-termo-de-cessao.blade.php
@@ -6,7 +6,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
       </div>
       <div class="modal-body">
-        <p>
+        <p style="text-align: justify;">
           AUTORIZO, no âmbito de entrevistas, fotos e vídeos produzidos pela Associação Brasileira de Agroecologia (ABA-Agroecologia), bem como instituições parceiras, apoiadoras e financiadoras do 13º Congresso Brasileiro de Agroecologia (CBA), durante as atividades desenvolvidas no âmbito do evento, o uso de minha imagem e voz em fotos e vídeos relacionados ao evento acima referido, sem finalidade comercial, pela ABA-Agroecologia e por quaisquer instituições parceiras, apoiadoras e financiadoras do 13º CBA. A presente autorização é concedida a título gratuito e por prazo Indeterminado, abrangendo o uso da imagem e voz no território nacional e no exterior, e em todas as suas modalidades, incluindo mas não se limitando a (i) folders, relatórios, cartazes, materiais de divulgação e afins; (ii) websites, TVs, redes sociais e qualquer outro tipo de mídia eletrônica. Por esta ser a expressão de minha vontade, declaro que autorizo o uso acima descrito sem que nada haja a ser reclamado a título de direitos conexos a minha imagem ou qualquer outro.
         </p>
       </div>
