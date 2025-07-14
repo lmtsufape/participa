23c9cf3206e9df4884acd381ce13056288d51bcf
Caio <caio.vsg@hotmail.com>
2025-07-14 16:33:09 -0300

    feat: Trocando csv por xlsx em expotar trabalhos

--- app/Exports/TrabalhosExport.php
+++ app/Exports/TrabalhosExport.php
@@ -5,13 +5,17 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class TrabalhosExport implements FromCollection, WithHeadings
+class TrabalhosExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
     use Exportable;
 
     protected $trabalhos;
 
+    /**
+     * Define os cabeçalhos das colunas no XLSX.
+     */
     public function headings(): array
     {
         return [
@@ -23,28 +27,25 @@ class TrabalhosExport implements FromCollection, WithHeadings
             'E-mail',
             'Telefone',
             'Co-autor(es)',
-            'CPF',
-            'E-mail',
-            'Telefone',
+            'CPF Co-autor',
+            'E-mail Co-autor',
+            'Telefone Co-autor',
         ];
     }
 
+    /**
+     * Recebe a coleção de trabalhos a ser exportada.
+     */
     public function __construct($trabalhos)
     {
         $this->trabalhos = $trabalhos;
     }
 
+    /**
+     * Retorna a coleção que será transformada em planilha.
+     */
     public function collection()
     {
         return $this->trabalhos;
-    }
-
-    public function getCsvSettings(): array
-    {
-        return [
-            'delimiter' => ';',
-            'line_ending' => ";\n",
-            'enclosure' => '',
-        ];
     }
 }
