9c1bbcddafe86a80a060094df6b23b9834019a21
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2025-11-17 12:12:23 -0300

    feat: adiciona a exibição da mensagem de erro

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.error')
 @section('content')
     <span class="font-25">500 | {{__('Server Error')}}</span>
+    @isset($exception)
+        <p><span class="font-25">{{$exception->getMessage()}}</span></p>
+    @endisset
 @endsection
