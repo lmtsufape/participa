4fe266cb7b2357b2a6b3f3f216844b72030d0d18
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2026-03-30 18:20:43 -0300

    Atualização do laravel 12 para o 13 e ajustes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 class Kernel extends HttpKernel
 {
     /**
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            PreventRequestForgery::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
