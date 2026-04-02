6040dedf45547ff4c1f9b00a004a139134cf1e49
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2026-04-02 19:47:38 -0300

    feat: adiciona a exibição da mensagem de erro

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.error')
 @section('content')
-    <span class="font-25">404 | {{__('Not Found')}}</span>
+    <span class="font-25">404 | {{ __('Not Found') }}</span>
+    @isset($exception)
+        <p><span class="font-25">{{ $exception->getMessage() }}</span></p>
+    @endisset
 @endsection
