07b95d6109339dd94149fd182de956297329a0da
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2026-04-02 19:20:49 -0300

    fix: filtra para pegar somente as atividades do evento

--- app/Http/Controllers/Submissao/EventoController.php
+++ app/Http/Controllers/Submissao/EventoController.php
@@ -2510,7 +2510,7 @@ class EventoController extends Controller
             ->orderBy('data')          // <-- garante que o banco já ordene por data
             ->get();
 
-        $atividades = Atividade::join('datas_atividades', 'atividades.id', '=', 'datas_atividades.atividade_id')
+        $atividades = Atividade::where('eventoId', $evento->id)->join('datas_atividades', 'atividades.id', '=', 'datas_atividades.atividade_id')
         ->select('atividades.*', 'datas_atividades.data', 'datas_atividades.hora_inicio')
         ->orderBy('datas_atividades.data')
         ->orderBy('datas_atividades.hora_inicio')
