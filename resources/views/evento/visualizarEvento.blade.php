2f679fbd7574b24dced14ac7e5306e8c5474640f
airton23 <airtondasf2305@gmail.com>
2026-08-22 06:01:06 -0300

    Ajuste na limitação de trabalhos no evento

--- resources/views/evento/visualizarEvento.blade.php
+++ resources/views/evento/visualizarEvento.blade.php
@@ -861,7 +861,14 @@
             <hr class="border-dark">
 
              <div class="col-12 d-flex justify-content-center align-items-center gap-2">
-                <a href="mailto:@if($evento->email){{ $evento->email }}@else{{ $evento->coordenador->email }}@endif" class="btn btn-my-secondary rounded-3">
+                @php
+                    $emailDestino = $evento->email ?? ($evento->coordenador->email ?? '');
+                    $assunto = urlencode('Contato sobre o evento: ' . $evento->nome);
+                @endphp
+
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to={{ $emailDestino }}&su={{ $assunto }}" 
+                target="_blank" 
+                class="btn btn-my-secondary rounded-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope me-1" viewBox="0 0 16 16">
                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
                     </svg>
